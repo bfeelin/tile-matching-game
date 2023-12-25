497967e9904cd7315942264b70e5505aeabe3437
Brian Phelan <brianphelan59@gmail.com>
2023-12-25 10:39:52 -0500

    Initalize grid, map through grid & handle flipping

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,38 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const tiles = ['a', 'c', 'a', 'b', 'a', 'b', 'c', 'c', 'b']
+
+const makeGameTiles = () => {
+  return tiles.map((tile) => {
+    return {
+      value: tile,
+      flipped: false,
+      matched: false
+    }
+  })
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentTiles, setCurrentTiles] = useState(makeGameTiles())
+
+  const handleFlipTile = (index:number) => {
+    let newTiles = [...currentTiles]
+    newTiles[index].flipped = !newTiles[index].flipped
+    setCurrentTiles(newTiles)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='tile-grid'>
+      {currentTiles?.map((tile, i) => (
+        <div 
+          className='tile'
+          key={i} 
+          onClick={() => handleFlipTile(i)}>
+          {tile.flipped ? tile.value : ''}
+        </div>
+      ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
